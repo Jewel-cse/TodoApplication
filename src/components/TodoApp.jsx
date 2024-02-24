b3636cb104f923bd846ed09ec71779ebc7bc6c85
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-25 00:08:20 +0600

    design errorPage ,if route is not defined then go to errorpage

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -9,6 +9,7 @@ export default function TodoApp() {
                     <Route path="/" element={<LoginComponent />}></Route>
                     <Route path="/login" element={<LoginComponent />}></Route>
                     <Route path="/welcome" element={<WellcomeComponent />}></Route>
+                    <Route path="*" element={<ErrorComponent />}></Route>
                 </Routes>
             </BrowserRouter>
             {/* <LoginComponent></LoginComponent> */}
@@ -63,6 +64,7 @@ export function LoginComponent() {
             )}
 
             <div className="LogingForm">
+                <h2>Please login </h2>
                 <div>
                     <label>User Name</label>
                     <input
@@ -93,4 +95,13 @@ export function LoginComponent() {
 
 export function WellcomeComponent() {
     return <div className="Wellcome">Wellcome component</div>;
+}
+export function ErrorComponent() {
+    return (
+        <div>
+            <h2>Your effort is uncompareable</h2>
+            <div className="errorPage">404 page not found!!</div>
+        </div>
+
+    );
 }
