1997dd12007315216b8e29cec03750434bc335a4
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-25 14:38:09 +0600

    create a hook in AuthContext.js - useAuth and use it to headerComponent

--- src/components/security/AuthContext.js
+++ src/components/security/AuthContext.js
@@ -1,12 +1,13 @@
 //1:create a context
 
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
+export const useAuth = ()=>useContext(AuthContext);
 //2: share the created context to some other components
 export default function AuthProvider({ children }) {
     //3: put some state in the context
-    const [number, setNumber] = useState(110);
-    setInterval(() => setNumber(number + 1), 5000); //number increament to each 5 seconds
+    const [number, setNumber] = useState(10);
+    setInterval(() => setNumber(number + 1), 10000); //number increament to each 5 seconds
     return <AuthContext.Provider value={{number}}>{children}</AuthContext.Provider>;
 }
