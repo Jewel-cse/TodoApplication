2df38ddd85a7bd5c22e9c393c586908516dbc3c6
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-25 17:25:30 +0600

    make secure route.if not authenticated then specific route can be visit

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -6,10 +6,19 @@ import LogoutComponent from "./Logout";
 import WellcomeComponent from "./Welcome";
 import ErrorComponent from "./Error";
 import ListTodosComponent from "./ListTodos";
-import AuthProvider from "../security/AuthContext";
+import AuthProvider, { useAuth } from "../security/AuthContext";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 export default function TodoApp() {
+
+  function AuthenticatedRoute({ children }) {
+    const authContext = useAuth()
+    if (authContext.isAuthenticated) {
+      return children ;
+    }
+    return <Navigate to="/"/>
+  }
+
   return (
     <div className="TodoApp">
       <AuthProvider>
@@ -18,9 +27,21 @@ export default function TodoApp() {
           <Routes>
             <Route path="/" element={<LoginComponent />} />
             <Route path="/login" element={<LoginComponent />} />
-            <Route path="/welcome/:username" element={<WellcomeComponent />} />
-            <Route path="/todos" element={<ListTodosComponent />} />
-            <Route path="/logout" element={<LogoutComponent />} />
+            <Route path="/welcome/:username" element={
+              <AuthenticatedRoute>
+                <WellcomeComponent />
+              </AuthenticatedRoute>
+            } />
+            <Route path="/todos" element={
+              <AuthenticatedRoute>
+                <ListTodosComponent />
+              </AuthenticatedRoute>
+            } />
+            <Route path="/logout" element={
+              <AuthenticatedRoute>
+                <LogoutComponent />
+              </AuthenticatedRoute>
+            } />
 
             <Route path="*" element={<ErrorComponent />} />
           </Routes>
