ee348a6a83b66d768fb6cf510dfbdcf57a778965
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-25 14:29:22 +0600

    update state with an interval

--- src/components/security/AuthContext.js
+++ src/components/security/AuthContext.js
@@ -7,6 +7,6 @@ export const AuthContext = createContext();
 export default function AuthProvider({ children }) {
     //3: put some state in the context
     const [number, setNumber] = useState(110);
-
+    setInterval(() => setNumber(number + 1), 5000); //number increament to each 5 seconds
     return <AuthContext.Provider value={{number}}>{children}</AuthContext.Provider>;
 }
