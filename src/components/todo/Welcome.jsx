10659c50fde389bc1483ed9ee1f5461efcbe3fb2
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-27 02:23:17 +0600

    get response back from back-end

--- src/components/todo/Welcome.jsx
+++ src/components/todo/Welcome.jsx
@@ -1,30 +1,36 @@
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import { useState } from "react";
 export default function WellcomeComponent() {
   const { username } = useParams();
+  const [message, setMessage] = useState();
   function callHelloWorlAPI() {
-    console.log("Clicked !!!")
-    axios.get("http://localhost:8080/hello-world-bean")
+    console.log("Clicked !!!");
+    axios
+      .get("http://localhost:8080/hello-world")
       .then((response) => successfulResponse(response))
-      .catch((error)=>errorResponse(error))
-      .finally(()=>console.log("Clean Up"));
+      .catch((error) => errorResponse(error))
+      .finally(() => console.log("Clean Up"));
   }
   function successfulResponse(response) {
     console.log(response);
+    setMessage(response.data);
   }
   function errorResponse(error) {
     console.log(error);
   }
   return (
-    
     <div>
       <h2>Wellcome Mr. {username}</h2>
       <div className="Wellcome">
         Manage your todos-<Link to="/todos">Go here</Link>
       </div>
       <div>
-        <button className="btn btn-success m-5" onClick={callHelloWorlAPI}>Call Hello World REST API</button>
+        <button className="btn btn-success m-5" onClick={callHelloWorlAPI}>
+          Call Hello World REST API
+        </button>
       </div>
+      <div className="text-info">{message}</div>
     </div>
   );
 }
