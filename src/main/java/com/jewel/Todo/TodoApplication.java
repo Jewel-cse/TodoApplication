04d1ce789113e364c57cc2fd111ee429ad968fd1
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-27 02:00:48 +0600

    add CORs mapping to call my back end REST api

--- src/main/java/com/jewel/Todo/TodoApplication.java
+++ src/main/java/com/jewel/Todo/TodoApplication.java
@@ -2,6 +2,9 @@ package com.jewel.Todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoApplication {
@@ -9,7 +12,21 @@ public class TodoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
+	//http://localhost:3000 to 8080
+	//Cross origin request (COR) not allow by default
+	//Allow all request only from http://localhost:3000
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000/");
+			}
+		};
+	}
 
 
 }
