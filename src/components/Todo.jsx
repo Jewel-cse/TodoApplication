052132b04bab5cb19a5f55cf88a06e08833091ef
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-27 19:44:14 +0600

    add a form for update using formik

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,32 +2,67 @@ import { useParams } from "react-router-dom";
 import { useAuth } from "./todo/security/AuthContext";
 import { retrieveTodoApi } from "./todo/api/TodoApiService";
 import { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
 
 export default function TodoComponent() {
     const { id } = useParams();
     const authContext = useAuth();
     const username = authContext.username;
-    const [description,setDescription] =  useState("")
+    const [description, setDescription] = useState("");
+    const [targetDate, setTargetDate] = useState("");
 
     useEffect(
-        ()=>retrieveTodo(),[id]    //id change holei retrieveTodo() call hobe, [id] dependency array
-    )
-
+        () => retrieveTodo(),
+        [id] //id change holei retrieveTodo() call hobe, [id] dependency array
+    );
 
     function retrieveTodo() {
-        retrieveTodoApi(username,id)
-        .then((response) =>
-            {
-               setDescription(response.data.description) 
-                console.log(response.data)
+        retrieveTodoApi(username, id)
+            .then((response) => {
+                setDescription(response.data.description);
+                setTargetDate(response.data.targetDate);
+                console.log(response.data);
             })
-        .catch((error) =>console.log(error))
+            .catch((error) => console.log(error));
+    }
+    function onSubmit(values) {
+        console.log(values)
     }
     return (
         <div className="container">
             <h2>Enter Todo Details</h2>
             <div>
-                Description: {description}
+                <Formik
+                    initialValues={{ description, targetDate }}
+                    enableReinitialize={true}
+                    onSubmit={onSubmit}
+                >
+                    {(props) => (
+                        <Form>
+                            <fieldset className="from-group">
+                                <label>Description</label>
+                                <Field
+                                    type="text"
+                                    className="form-control"
+                                    name="description"
+                                ></Field>
+                            </fieldset>
+                            <fieldset className="from-group">
+                                <label>TargetDate</label>
+                                <Field
+                                    type="date"
+                                    className="form-control"
+                                    name="targetDate"
+                                ></Field>
+                            </fieldset>
+                            <div>
+                                <button className="btn btn-success m-5" type="submit">
+                                    Save
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </div>
     );
