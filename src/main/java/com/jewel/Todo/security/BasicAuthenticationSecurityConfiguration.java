0d14903697ffbbea11f93f6c656228ff3d2da0e3
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-29 01:45:24 +0600

    basic authentication: gives popUp if unathorized

--- src/main/java/com/jewel/Todo/security/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/com/jewel/Todo/security/BasicAuthenticationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.jewel.Todo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +15,10 @@ public class BasicAuthenticationSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth->auth.anyRequest().authenticated()
+        );
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
